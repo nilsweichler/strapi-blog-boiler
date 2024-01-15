7934f93f0badeaa92631baa962ae4f3e26ee4048
Nils Weichler <nils.weichler@hotmail.de>
2024-01-15 20:54:26 +0700

    changed database client

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,18 @@
 // path: ./config/env/production/database.js
 
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapidb"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },
-      debug: false,
     },
-  };
-};
+    debug: false,
+  },
+});
